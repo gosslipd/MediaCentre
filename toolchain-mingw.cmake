1678be4b346ffb70d88d22d39b966d64ddffdff3
gosslipd <peter.gossling@gmail.com>
2025-03-03 11:39:40 +0000

    MSVC build all working, MinGW build just not linking - forget it as GStreamer issue?

---
+++ toolchain-mingw.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_C_COMPILER "C:/Qt/Tools/llvm-mingw1706_64/bin/clang.exe")
+set(CMAKE_CXX_COMPILER "C:/Qt/Tools/llvm-mingw1706_64/bin/clang++.exe")
+set(CMAKE_CXX_FLAGS "-std=c++17" CACHE STRING "C++ flags" FORCE)
